319cdebeca042e7afcf8c2e70670516c3255fd78
aibin <1149299088@qq.com>
2017-10-09 23:18:17 +0800

    交互：添加 删除 标记

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
 import React, { Component } from 'react';
+import 'normalize.css';
+import './reset.css';
 import './App.css';
-import TodoInput from 'TodoInput'
-import TodoItem from 'TodoItem'
+import TodoInput from './TodoInput';
+import TodoItem from './TodoItem';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      newTodo: 'test',
+      newTodo: '',
       todoList: [
-        {id: 1, title: 'first to do'},
-        {id: 2, title: 'second to do'}
+
       ]
-    }
+    };
   }
   render() {
-    let todos = this.state.todoList.map((item, index)=> {
+    let todos = this.state.todoList
+      .filter((item)=> !item.deleted)
+      .map((item, index)=> {
 	    return (
-        <li>
-          <TodoItem todo={item}/>
+        <li key={index}>
+          <TodoItem todo={item}
+                    onToggle={this.toggle.bind(this)}
+                    onDelete={this.delete.bind(this)}/>
         </li>
 	    )
     });
+
     return (
       <div className="App">
         <h1>My To Do</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo}/>
+          <TodoInput content={this.state.newTodo}
+                     onChange={this.changeTitle.bind(this)}
+                     onSubmit={this.addTodo.bind(this)}/>
         </div>
         <ol>
           {todos}
@@ -34,6 +42,38 @@ class App extends Component {
       </div>
     );
   }
+  toggle(e, todo) {
+    todo.status = todo.status === 'completed' ? '' : 'completed';
+    this.setState(this.state);
+  }
+  changeTitle(event) {
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    });
+  }
+  addTodo(event) {
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    });
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    });
+  }
+  delete(event, todo) {
+    todo.deleted = true;
+    this.setState(this.state);
+  }
 }
 
 export default App;
+
+let id = 0;
+function idMaker() {
+  id += 1;
+  return id;
+}
