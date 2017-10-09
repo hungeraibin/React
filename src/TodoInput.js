27782a7c485248950cc87d926aa27362a7797ab2
aibin <1149299088@qq.com>
2017-10-09 23:54:51 +0800

    add css

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
+import './TodoInput.css';
 
 export default class TodoInput extends Component {
 	render() {
-		return <input type="text" value={this.props.content}
+		return <input type="text" className="TodoInput"
+		              value={this.props.content}
 		              onChange={this.changeTitle.bind(this)}
 		              onKeyPress={this.submit.bind(this)}/>
 	}
