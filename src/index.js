39695242b9821b1ba411cc188bb9e46cbad6acce
aibin <1149299088@qq.com>
2017-10-09 19:53:48 +0800

    基本框架完成

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <h1>Hi, world</h1>,
-    document.getElementById('root2')
+    document.getElementById('root')
 );
